a81f2dbb7e47dd0807ec9c7d7a2fe943f87368a4
Mikolaj Mindykowski <mikolaj.mindykowski@gmail.com>
2023-03-31 16:59:44 +0200

    js sprawdzenie liczby

---
+++ main.js
@@ -0,0 +1,8 @@
+function number (n) {
+    if ( n > 100 && n <= 200) {
+        return 'Liczba znajduje się w przedziale';
+  }   else {
+        return 'Liczba nie znajduje się w przedziale';
+  }
+}
+console.log(number(115));
